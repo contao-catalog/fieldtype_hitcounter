b53288c01450aa27ddd61384768720367d016a48
Christian Schiffler <c.schiffler@cyberspectrum.de>
2009-10-22 16:20:48 +0000

    Added check that only one hit per IP and day is counted instead of every reload - hope it is more representative that way.
    git-svn-id: http://typolight-catalog.googlecode.com/svn@64 5f8f583f-444c-0410-842f-6b1beb028a02

--- src/system/modules/catalogitemhitsfield/CatalogHitsField.php
+++ src/system/modules/catalogitemhitsfield/CatalogHitsField.php
@@ -29,10 +29,20 @@ class CatalogHitsField extends Backend {
 		// note the $raw will always be one smaller than the real amount in the db. We can not do anything about this as the raw is not a pointer.
 		if($objCatalogInstance instanceof ModuleCatalogReader)
 		{
-			$objCatTable=$this->Database->prepare("SELECT tableName FROM tl_catalog_types WHERE id=?")
-							->execute($objCatalog->pid);
-			$this->Database->prepare("UPDATE " . $objCatTable->tableName . " SET " . $k . "=" . $k . "+1 WHERE id=?")
-							->execute($objCatalog->id, $k);
+			// allow only one hit per item and ip per day.
+			$haveHit=$this->Database->prepare("SELECT id, ip FROM tl_catalog_hitcounter WHERE cat_id=? AND item_id=? AND ip=? AND time>=?")
+								->execute($objCatalog->pid, $objCatalog->id, $_SERVER['REMOTE_ADDR'], (time()-(60*60*24)))
+								->next();
+			if(!$haveHit->numRows)
+			{
+				$objCatTable=$this->Database->prepare("SELECT tableName FROM tl_catalog_types WHERE id=?")
+								->execute($objCatalog->pid);
+				$this->Database->prepare("UPDATE " . $objCatTable->tableName . " SET " . $k . "=" . $k . "+1 WHERE id=?")
+								->execute($objCatalog->id, $k);
+				$this->Database->prepare("INSERT INTO tl_catalog_hitcounter (cat_id, item_id, ip, time) VALUES (?, ?, ?, ?)")
+							   ->execute($objCatalog->pid, $objCatalog->id, $_SERVER['REMOTE_ADDR'], time());
+							   // Would love to use $this->Environment->ip here but this is the internal IP of an network if behind NAT, therefore useless.
+			}
 		}
 		$html="<span class=\"hitcounter\">" . sprintf($GLOBALS['TL_LANG']['cataloghitsfieldfield']['hitsvalue'], $raw) . "</span>";
 		return array
---
+++ src/system/modules/catalogitemhitsfield/config/database.sql
@@ -0,0 +1,29 @@
+-- **********************************************************
+-- *                                                        *
+-- * IMPORTANT NOTE                                         *
+-- *                                                        *
+-- * Do not import this file manually but use the TYPOlight *
+-- * install tool to create and maintain database tables!   *
+-- *                                                        *
+-- **********************************************************
+
+
+-- --------------------------------------------------------
+
+--
+-- Table `tl_catalog_hitcounter`
+-- 
+
+CREATE TABLE `tl_catalog_hitcounter` (
+-- id for this entry
+  `id` int(10) unsigned NOT NULL auto_increment,
+-- id of the catalog
+  `cat_id` int(10) unsigned NOT NULL default '0',
+-- id of the item in the catalog 
+  `item_id` int(10) unsigned NOT NULL default '0',
+-- ip where this hit originated from
+  `ip` varchar(255) NOT NULL default '',
+-- time when this hit occured
+  `time` int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) TYPE=MyISAM;
